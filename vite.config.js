92643537ecbea7bd2df754a5c4d9788cb65713e9
Mark Louise Gardiola <81681337+GardiolaMarkLouise@users.noreply.github.com>
2025-06-09 11:28:49 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  base: './', // ← This tells Vite to use relative paths
+})
